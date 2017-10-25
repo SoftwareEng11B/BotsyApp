2901156fd7f6cfc50ce3d21b3bf9b72be5413a08
Kemley <kemley@ufl.edu>
2017-10-25 12:45:58 -0400

    Did controller

--- modules/users/client/controllers/mural_request.clienter.controller.js
+++ modules/users/client/controllers/mural_request.clienter.controller.js
@@ -5,32 +5,5 @@ angular.module('users').controller('MuralRequestController', ['$scope', '$state'
 
     // Get an eventual error defined in the URL query string:
     $scope.error = $location.search().err;
-
-    $scope.contactform = function(isValid){
-
-      $scope.error = null;
-
-      if (!isValid) {
-        $scope.$broadcast('show-errors-check-validity', 'userForm');
-
-        return false;
-      }
-
-      var data = ({
-        contactName : $scope.userForm.firstName.$$rawModelValue,
-        contactEmail : $scope.userForm.email.$$rawModelValue,
-        contactMsg : $scope.userForm.message.$$rawModelValue
-      });
-
-      $http.post('/api/auth/contact-us', data).
-        success(function(data, status, headers, config) {
-          console.log(1);
-          $state.go('home', $state.previous.params);
-        }).
-        error(function(data, status, headers, config) {
-          console.log(0);
-        });
-
-    };
   }
 ]);
