b8ae1e329a7f9ddc0a84f51ea92b03f911980882
jLouis <jeandlouis1@gmail.com>
2017-10-26 18:55:10 -0400

    created backend set up for the wall

--- modules/walls/server/models/wall.server.model.js
+++ modules/walls/server/models/wall.server.model.js
@@ -20,103 +20,153 @@ var WallSchema = new Schema({
     type: Date,
     default: Date.now
   },
-  user: {
-    type: Schema.ObjectId,
-    ref: 'User'
-  }
-  /*  created: {
-    type: Date,
-    default: Date.now
-  },
-  user: {
-    type: Schema.ObjectId,
-    ref: 'User'
-  }
 
-   Wall_type: {
-    type: [{
-      type: String,
-      enum: ['custom', 'other']
-    }],
-    default: ['custom'],
+  user: {
+    type: Schema.ObjectId,
+    ref: 'User'
+  },
+
+
+  wall_info: {
+    
+      length: {
+        type: String, 
+        required: true,
+        default:"1"
+      },
+
+      height: {
+        type: String, 
+        required: true,
+        default:"1"
+      },
+
+      loc_type: {
+        type: String, 
+        required: true,
+        default: 'home',
+      },
+
+      material: {
+        type: String, 
+        required: true,
+        default:"none"
+      },
+
+      finish: {
+        type: String, 
+        required: true,
+        default:"none"
+      },
+
+      paint: {
+        type: String, 
+        required: true,
+        default:"none"
+      },
+
+      paint_type: {
+        type: String, 
+        required: true,
+        default:"none"
+      },
+
+      prep_req: {
+        type: String, 
+        required: false,
+        default:"none"
+      },
+
+      prep: {
+        type: String, 
+        required: true,
+        default:"none"
+      },
+
+      paint_req: {
+          type: String, 
+          required: true,
+          default:"none"
+      },
+      recolor: {
+        type: String, 
+        required: true,
+      default:"none"
+      },
+      
+  },
+  
+
+  Wall_type: {
+    type: String, 
+    required: true,
+    default: 'custom',
     //required: 'Please provide at least one role'
   },
 
-  plain_Wall_Img: {
-    type: String,
-    default: 'modules/walls/client/img/defaultWall.png'
-  },
+  Wall_Imges: {
+    plain_Wall_Img: {
+      type: String,
+      default: 'modules/walls/client/img/defaultWall.png'
+    },
 
-  Diminsioned_Wall_Img: {
-    type: String,
-    default: 'modules/walls/client/img/defaultWall.png'
-  },
+    Diminsioned_Wall_Img: {
+      type: String,
+      default: 'modules/walls/client/img/defaultWall.png'
+    },
 
-  Wall_with_SVG_Img: {
-   type: String,
-    default: 'modules/walls/client/img/defaultWall.png'
-  },
+    Wall_with_SVG_Img: {
+     type: String,
+      default: 'modules/walls/client/img/defaultWall.png'
+    },
 
-  svg_Img: {
-    type: String,
-    default: 'modules/walls/client/img/defaultSVGs.jpg'
+    svg_Img: {
+      type: String,
+      default: 'modules/walls/client/img/defaultSVGs.jpg'
+    },
   },
 
   status: {
-      {},
-      {},
-      {}
+    
+      paid: {
+        type: Boolean,
+        default: false,
+      },
+      
+      matched: {
+        type: Boolean,
+        default: false,
+      },  
+      
+      completed: {
+        type: Boolean,
+        default: false,
+      },
+
+      hasQuote: {
+        type: Boolean,
+        default: false,
+      },
+
+      comment: {
+        type: String, 
+        default:''
+      },
+    
   },
 
- /* Artist: {
-    type: String,
-    trim: true,
-    default: '',
-    validate: [validateLocalStrategyProperty, 'Please fill in your first name']
-  },
-*//*
-  Wall_info: {
-    type: String,
-    trim: true,
-    default: '',
-    validate: [validateLocalStrategyProperty, 'Please fill in your first name']
-  },
-
-
-
-
-
-  lastName: {
-    type: String,
-    trim: true,
-    default: '',
-    validate: [validateLocalStrategyProperty, 'Please fill in your last name']
-  },
-  displayName: {
-    type: String,
-    trim: true
-  },
-  email: {
-    type: String,
-    unique: true,
-    lowercase: true,
-    trim: true,
-    default: '',
-    validate: [validateLocalStrategyEmail, 'Please fill a valid email address']
-  },
-  username: {
-    type: String,
-    unique: 'Username already exists',
-    required: 'Please fill in a username',
-    lowercase: true,
-    trim: true
-  },
  
-  
+   
+
+  Artist: {
+   type: Schema.ObjectId,
+    ref: 'User'
+  },
+
 
   updated: {
     type: Date 
-  },*/
+  }
 
 });
 
