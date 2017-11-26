0cc4385ffd20b3dc6ddbcbd0f81f7e9fa95124f9
Jon Legaspi <fairview1036@gmail.com>
2017-11-26 00:20:53 -0500

    image saving testing

--- modules/walls/client/controllers/walls.client.controller.js
+++ modules/walls/client/controllers/walls.client.controller.js
@@ -17,6 +17,56 @@
     vm.form = {};
     vm.remove = remove;
     vm.save = save;
+    vm.progress = 0;
+
+
+
+
+
+    vm.upload = function (dataUrl) {
+      
+            Upload.upload({
+              url: '/api/users/picture',
+              data: {
+                newProfilePicture: dataUrl
+              }
+            }).then(function (response) {
+              $timeout(function () {
+                onSuccessItem(response.data);
+              });
+            }, function (response) {
+              if (response.status > 0) onErrorItem(response.data);
+            }, function (evt) {
+              vm.progress = parseInt(100.0 * evt.loaded / evt.total, 10);
+            });
+          };
+      
+          
+    // Called after the user has successfully uploaded a new picture
+    function onSuccessItem(response) {
+      // Show success message
+      Notification.success({ message: '<i class="glyphicon glyphicon-ok"></i> Successfully changed Wall picture' });
+
+      // Populate user object
+      vm.user = Authentication.user = response;
+
+      // Reset form
+      vm.fileSelected = false;
+      vm.progress = 0;
+    }
+
+    // Called after the user has failed to upload a new picture
+    function onErrorItem(response) {
+      vm.fileSelected = false;
+      vm.progress = 0;
+
+      // Show error message
+      Notification.error({ message: response.message, title: '<i class="glyphicon glyphicon-remove"></i> Failed to change Wall picture' });
+    }
+
+
+
+
 
     // Remove existing Wall
     function remove() {
@@ -49,5 +99,6 @@
         vm.error = res.data.message;
       }
     }
+    
   }
 }());
